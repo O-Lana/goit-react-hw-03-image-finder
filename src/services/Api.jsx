da153c6412770f9ca1e3cd983511b3564f8b1626
Lana Ovcharenko <lana.ovchar@gmail.com>
2022-02-07 21:59:34 +0200

    fix

--- src/services/Api.jsx
+++ src/services/Api.jsx
@@ -1,13 +1,3 @@
-// import axios from 'axios';
-
-// const API_KEY = '24504090-67d4d1d2d94058f1108b78b7b';
-// axios.defaults.baseURL = 'https://pixabay.com/api/';
-
-// export const getImages = async (data, page) => {
-//   const response = await axios.get(`?q=${data}&page=${page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`);
-//   return response.data;
-// };
-
 function fetchImages(query, page) {
 
   const API_KEY = '24504090-67d4d1d2d94058f1108b78b7b';
